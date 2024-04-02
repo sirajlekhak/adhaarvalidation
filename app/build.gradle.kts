c59077af343baa4d330691907409cee9c22108ff
Siraj Alam <crajmusics@gmail.com>
2024-04-02 11:31:39 +0530

    adhaar validation updates 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 
 android {
     compileSdkVersion(34)
-
     defaultConfig {
         applicationId = "com.app.myapplication"
         minSdkVersion(24)
@@ -13,37 +12,33 @@ android {
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        namespace = "com.app.myapplication"
     }
-
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    // Add namespace declaration here
+    namespace = "com.app.myapplication"
 }
 
 dependencies {
-    implementation("androidx.camera:camera-core:1.1.0-alpha10")
-    implementation("androidx.camera:camera-camera2:1.1.0-alpha10")
-
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.4.0")
-    implementation("com.google.android.material:material:1.5.0-alpha02")
-    implementation("androidx.activity:activity-ktx:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
-
+    implementation("androidx.camera:camera-core:1.3.2")
+    implementation("androidx.camera:camera-camera2:1.3.2")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material3:1.0.0-alpha03")
+    implementation("androidx.activity:activity-ktx:1.8.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
